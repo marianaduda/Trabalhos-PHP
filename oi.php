338ef95e7d8d1927574b2a5a5868d0ba8ad065d6
marianaduda <marianaeduarda197@gmail.com>
2025-05-14 11:38:09 -0300

    Add files via upload

---
+++ oi.php
@@ -0,0 +1,18 @@
+<h1>Minha Primeira Aula de PHP</h1>
+
+<?php
+//Exibindo uma mensagem na tela
+echo "Olá, Mundo!";
+echo "<br>";
+
+$nome = "Mariana"; // string
+$idade = 17; // inteiro
+$altura = 1.83; // float
+$peso = 65; // float
+
+//Exibindo o valor de uma variavel
+echo "Nome: $nome <br>";
+echo "Idade: $idade <br>";
+echo "Altura: $altura <br>";
+echo "Peso: $peso <br>";
+?>
